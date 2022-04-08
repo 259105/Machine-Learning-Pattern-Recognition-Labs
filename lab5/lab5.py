1721f33ce58ee3cc1238a242103f845b00a2b182
259105 <fbmichele@hotmail.it>
2022-04-08 19:39:42 +0200

    prova push

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -3,7 +3,7 @@ import scipy
 
 def loadIrisDataset() :
     import sklearn.datasets;
-    return sklearn.datasets.load_iris()["data"].T, sklearn.datasets.load_iris()["target"];
+    ##return sklearn.datasets.load_iris()["data"].T, sklearn.datasets.load_iris()["target"];
 
 if __name__ == "__main__" :
     
