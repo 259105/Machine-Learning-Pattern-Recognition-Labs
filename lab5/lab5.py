aed0dfa3509fe184c99909474e4f1f1a9751898e
MICHELE CERRA s301117 <s301117@studenti.polito.it>
2022-04-08 19:29:00 +0200

    Added funct load dataset

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,2 +1,9 @@
+import numpy
+import scipy
+
+def loadIrisDataset() :
+    import sklearn.datasets;
+    return sklearn.datasets.load_iris()["data"].T, sklearn.datasets.load_iris()["target"];
+
 if __name__ == "__main__" :
     
