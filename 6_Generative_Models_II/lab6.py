ec6443dcb4b5890f30e3e4ebf161ccdc5a5f9b4c
259105 <fbmichele@hotmail.it>
2022-04-30 16:50:28 +0200

    start lab6

--- 6_Generative_Models_II/lab6.py
+++ 6_Generative_Models_II/lab6.py
@@ -1,4 +1,19 @@
 import numpy
+import string
+import scipy.special
+import itertools
+import sys
+import data.load as load
 
-if __name__ == "__main__" :
-    
+
+def mcol(v):
+    return v.reshape((v.size, 1))
+
+
+if __name__ == "__main__":
+    lInf, lPur, lPar = load.load_data()
+
+    lInf_train, lInf_evaluation = load.split_data(lInf, 4)
+    lPur_train, lPur_evaluation = load.split_data(lPur, 4)
+    lPar_train, lPar_evaluation = load.split_data(lPar, 4)
+
